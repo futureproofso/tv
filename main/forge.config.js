560ef5540f72e341b860bafc3433e2eef0909072
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2023-04-28 22:02:21 -0400

    fix: copy moment timezone json files

--- main/forge.config.js
+++ main/forge.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  packagerConfig: {},
-  rebuildConfig: {},
+  packagerConfig: {
+    ignore: ['test', 'storage', 'moment-timezone-data'],
+    prune: true,
+  },
+  rebuildConfig: {
+    force: false
+  },
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
@@ -30,5 +35,19 @@ module.exports = {
         prerelease: false
       }
     }
-  ]
+  ],
+  hooks: {
+    packageAfterPrune: async (forgeConfig, buildPath, electronVersion, platform, arch) => {
+      console.log('running after prune...');
+      const path = require('path')
+      const fs = require('fs')
+      const source = (subdir) => `./moment-timezone-data/${subdir}/latest.json`;
+      const dest = (subdir) => `node_modules/moment-timezone/data/${subdir}/latest.json`;
+      return new Promise((resolve, reject) => {
+        fs.copyFileSync(source('meta'), path.resolve(buildPath, dest('meta')));
+        fs.copyFileSync(source('packed'), path.resolve(buildPath, dest('packed')));
+        resolve();
+      })
+    },
+  }
 };
