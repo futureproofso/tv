5196a513239bc0dac7bb8627d8190fa25a69d079
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2023-05-18 11:58:19 -0400

    fix(desk): rm sqlite3 build for code sign
    and copy dotenv module

--- main/forge.config.js
+++ main/forge.config.js
@@ -1,60 +1,122 @@
+const path = require("path");
+const { promises: fs } = require("fs");
+
+const env = require('./env.js');
+
+const identity = env.APPLE_CODESIGN_IDENTITY;
+const appleId = env.APPLE_ID;
+const appleIdPassword = env.APPLE_PASSWORD;
+const teamId = env.APPLE_TEAM_ID;
+
+/**
+ * Copies a directory into a new location recursively.
+ *
+ * @param {string} src Path to directory you want to copy
+ * @param {string} dest New location name for directory being created
+ */
+async function copyDir(src, dest) {
+  await fs.mkdir(dest, { recursive: true });
+  let entries = await fs.readdir(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+    let srcPath = path.join(src, entry.name);
+    let destPath = path.join(dest, entry.name);
+
+    entry.isDirectory()
+      ? await copyDir(srcPath, destPath)
+      : await fs.copyFile(srcPath, destPath);
+  }
+}
+
 module.exports = {
   packagerConfig: {
-    ignore: ['test', 'storage', 'moment-timezone-data', '.env'],
+    ignore: [
+      "test",
+      "storage",
+      "moment-timezone-data",
+      "env.js",
+      "node_modules/dotenv",
+    ],
     prune: true,
-    osxSign: {},
+    osxSign: {
+      identity
+    },
     osxNotarize: {
-      tool: 'notarytool',
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_PASSWORD,
-      teamId: process.env.APPLE_TEAM_ID,
-    }
+      tool: "notarytool",
+      appleId,
+      appleIdPassword,
+      teamId,
+    },
   },
   rebuildConfig: {
-    force: false
+    force: true,
   },
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   publishers: [
     {
-      name: '@electron-forge/publisher-github',
+      name: "@electron-forge/publisher-github",
       config: {
         repository: {
-          owner: 'futureproofso',
-          name: 'tv'
+          owner: "futureproofso",
+          name: "tv",
         },
-        prerelease: false
-      }
-    }
+        prerelease: false,
+      },
+    },
   ],
   hooks: {
-    packageAfterPrune: async (forgeConfig, buildPath, electronVersion, platform, arch) => {
-      console.log('running after prune...');
-      const path = require('path')
-      const fs = require('fs')
-      const source = (subdir) => `./moment-timezone-data/${subdir}/latest.json`;
-      const dest = (subdir) => `node_modules/moment-timezone/data/${subdir}/latest.json`;
-      return new Promise((resolve, reject) => {
-        fs.copyFileSync(source('meta'), path.resolve(buildPath, dest('meta')));
-        fs.copyFileSync(source('packed'), path.resolve(buildPath, dest('packed')));
+    packageAfterPrune: async (
+      forgeConfig,
+      buildPath,
+      electronVersion,
+      platform,
+      arch
+    ) => {
+      const dotenvSrc = "node_modules/dotenv";
+      const dotenvOut = path.resolve(buildPath, "node_modules/dotenv");
+      const momentSrc = (subdir) => {
+        return `./moment-timezone-data/${subdir}/latest.json`;
+      };
+      const momentOut = (subdir) => {
+        return path.resolve(
+          buildPath,
+          `node_modules/moment-timezone/data/${subdir}/latest.json`
+        );
+      };
+      const prodEnvSrc = ".env.prod";
+      const prodEnvOut = path.resolve(buildPath, ".env.prod");
+      const sqlite3BuildPath = path.resolve(
+        buildPath,
+        "node_modules/sqlite3/build"
+      );
+      return new Promise(async (resolve, reject) => {
+        await copyDir(dotenvSrc, dotenvOut);
+        await fs.copyFile(prodEnvSrc, prodEnvOut);
+        await fs.copyFile(momentSrc("meta"), momentOut("meta"));
+        await fs.copyFile(momentSrc("packed"), momentOut("packed"));
+        await fs.rm(sqlite3BuildPath, {
+          recursive: true,
+          force: true,
+        });
         resolve();
-      })
+      });
     },
-  }
+  },
 };
